211a6d803b82cf06470a1b0dd1610b9af7c58cb4
whattrash <21ss0676@wwprsd.org>
2019-06-15 14:35:04 -0400

    basic commands

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
+from discord.ext import commands
 import discord
 import json
 import os
-path = os.path.abspath('awoo\TOKEN.json')
-bot = discord.Client()
-
+path = os.path.abspath('TOKEN.json')
+bot = commands.Bot(command_prefix="~")
+bot.remove_command('help')
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
     else:
-        print(message.author + ": " + message.content)
+        print(str(message.author) + ": " + str(message.content))
+        await bot.process_commands(message)
+
+
+@bot.command()
+async def ping(ctx):
+    '''awooo'''
+    lat = bot.latency
+    await ctx.send(lat)
+
+@bot.command()
+async def hello(ctx):
+    embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+    embed.description = "hello"
+    embed.set_author(name=bot.user)
+
+    await ctx.send(embed=embed)
 
 
 with open(path, 'r') as f:
